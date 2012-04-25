b68df028bccc1a2180e020be562d18b686245c13
Daniel Razafimandimby <daniel.dimby@gmail.com>
2012-04-25 08:57:04 +0200

    First commit

---
+++ src/no/oxycoon/android/rebus/RebusActivity.java
@@ -0,0 +1,13 @@
+package no.oxycoon.android.rebus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RebusActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
